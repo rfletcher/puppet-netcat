4a16f7c6a4310fd11f7ff8abd6fceaeb136e3e4a
Andrew Stiegmann (stieg) <andrew.stiegmann@flixster-inc.com>
2015-02-15 23:49:54 -0600

    Add support for CentOS 7, add improved structure
    Addes support for CentOS 7 since they apparently don't like the
    standard netcat name (jerks).  Instead they use nmap-ncat.  So
    fix that and add better structure for parameters as per the puppet
    way.
    
    Also cleanus up variable names a bit so meaning is clearer and more
    standardized.

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,7 +11,7 @@
 # === Examples
 #
 #  class { 'netcat':
-#    ensure => latest,
+#    package_ensure => latest,
 #  }
 #
 # === Authors
@@ -23,9 +23,14 @@
 # Copyright 2014 Rick Fletcher
 #
 class netcat (
-  $ensure = 'present',
-) {
-  package { 'netcat':
-    ensure => $ensure,
+  $package_ensure = $netcat::params::package_ensure,
+  $package_name   = $netcat::params::package_name,
+) inherits netcat::params {
+
+  package { 'netcat' :
+    allow_virtual => false,
+    ensure        => $package_ensure,
+    name          => $package_name,
   }
+
 }
